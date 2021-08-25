17eaa139db7100991158ad2e3d96f7fdbbf53c22
CivilTT <civiltt.ss.@gmail.com>
2021-08-25 20:53:26 +0900

    Update ShareInventory

---
+++ pull.bat
@@ -0,0 +1,5 @@
+@echo off
+git config --global user.name CivilTT
+git config --global user.email civiltt.ss@gmail.com
+git pull https://CivilTT@github.com/CivilTT/ShareInventory
+pause
---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo off
+git config --global user.name CivilTT
+git config --global user.email civiltt.ss@gmail.com
+git add -A
+git commit -m "Update ShareInventory"
+git push https://CivilTT@github.com/CivilTT/ShareInventory
+pause
